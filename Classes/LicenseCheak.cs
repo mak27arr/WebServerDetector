47522969cb06fc7bee044250effae9c8271749e8
makar <mak27arr@gmail.com>
2019-11-20 14:21:56 +0200

    dns add commit

--- Classes/LicenseCheak.cs
+++ Classes/LicenseCheak.cs
@@ -10,7 +10,7 @@ namespace WebServerDetector.Classes
     {
         public static void Cheak()
         {
-            DateTime valid_until = new DateTime(2019, 11, 21, 14, 00, 00);
+            DateTime valid_until = new DateTime(2019, 11, 21, 18, 00, 00);
             if (DateTime.Compare(DateTime.Now, valid_until) >= 0 || DateTime.Compare(GetNetworkTime(), valid_until) >= 0)
                 throw new Exception("You dont pay for this program! Be happy)");
         }
